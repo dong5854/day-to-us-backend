491bfdee330c943899d22f600af1be016ccd4436
dong young <dong5854@gmail.com>
2025-12-29 18:54:06 +0900

    first commit

---
+++ src/main/kotlin/com/dong/day_to_us/DayToUsApplication.kt
@@ -0,0 +1,11 @@
+package com.dong.day_to_us
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DayToUsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DayToUsApplication>(*args)
+}
---
+++ src/test/kotlin/com/dong/day_to_us/DayToUsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dong.day_to_us
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DayToUsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
