b7d4bb1e8987974cc06e4fcc1f6ee6acb1c1c4c8
dong young <dong5854@gmail.com>
2026-01-01 02:03:10 +0900

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,17 +20,17 @@ repositories {
     mavenCentral()
 }
 
-// TODO: DB 구성 후 주석 해제하기
 dependencies {
-// 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("tools.jackson.module:jackson-module-kotlin")
-// 	runtimeOnly("org.postgresql:postgresql")
-// 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 kotlin {
