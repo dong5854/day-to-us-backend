7737601d53cbb68d1091268c757104d4bd4e6278
dong young <dong5854@gmail.com>
2026-01-02 03:29:56 +0900

    build: validation 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("tools.jackson.module:jackson-module-kotlin")
     runtimeOnly("org.postgresql:postgresql")
